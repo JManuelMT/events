cff98bc08b676094b5d2e63015a07361298463eb
Jose Manuel Mendez <josemmendezt@gmail.com>
2019-11-20 04:39:24 -0600

    Updates

---
+++ static/js/5.25e360ff.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpevents-organization"]=this["webpackJsonpevents-organization"]||[]).push([[5],{157:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(11),i=(e(3),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.m)((function(){return r.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.25e360ff.chunk.js.map
